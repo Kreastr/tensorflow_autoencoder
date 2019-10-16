2f0ead0e99721373340ed96b53d32583c003f2b1
Aleksei Romanenko <aleksei.romanenko@lut.fi>
2019-10-16 19:02:56 +0300

    setup config for package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+install_requires = [ 'tensorflow', 'jsonpickle', 'sklearn' ]
+
+setup(name='tfautoencoder',
+    version='0.1',
+    author='Aleksei Romanenko',
+    author_email='aleksei.romanenko@pm.me',
+    platform='noarch',
+    license='GPLv3',
+    description='Tensorflow Model builder for general purpose Autoencoder. ',
+    long_description="""Tensorflow Model builder for general purpose Autoencoder.""",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    url='https://github.com/Kreastr/tensorflow_autoencoder',
+    py_modules=['src/__init__'],
+    install_requires=install_requires
+    )
